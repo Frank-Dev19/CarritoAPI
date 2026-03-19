ed8607919ed1f1025c9cc2daf2c7e1894bc39b6f
Frank Jhosep <franksanchez.1619@gmail.com>
2026-03-19 09:03:11 -0500

    Estructura inicial de proyecto con usuarios, productos y  carrito

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CartModule, ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
